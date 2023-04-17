c679d7b1673df88e3d81644ee165ad14e420bbd4
/dev/null <a.d.kamenev@yandex.ru>
2023-04-17 16:31:32 +0300

    complete(?)

--- OOP_Lesson5_MVC-master/src/personal/model/UserMapper.java
+++ OOP_Lesson5_MVC-master/src/personal/model/UserMapper.java
@@ -5,8 +5,17 @@ public class UserMapper {
         return String.format("%s,%s,%s,%s", user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
     }
 
+//    public String map2(User user) {
+//        return String.format("%s;%s;%s;%s\n", user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
+//    }
+
     public User map(String line) {
         String[] lines = line.split(",");
         return new User(lines[0], lines[1], lines[2], lines[3]);
     }
+
+//    public User map2(String line) {
+//        String[] lines = line.split(";");
+//        return new User(lines[0], lines[1], lines[2], lines[3]);
+//    }
 }
